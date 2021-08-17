33d07f80c80a1fa31fc15d75a311891b13c61a88
Neal Fultz <nfultz@gmail.com>
2021-08-17 00:05:22 +0000

    path issue

--- ultrafilter.sh
+++ ultrafilter.sh
@@ -1,7 +1,7 @@
 #1/bin/bash
 google-chrome --headless --user-data-dir=/home/nfultz/.config/google-chrome/ --profile-directory="Profile 1" --dump-dom "https://forums.somethingawful.com/showthread.php?threadid=3938867&goto=lastpost" > ultrafilter.html
 
-xmlstarlet fo -H ultrafilter.html 2>/dev/null | xmlstarlet tr rss.xsl >test.rss
+xmlstarlet fo -H ultrafilter.html 2>/dev/null | xmlstarlet tr /home/nfultz/projects/ultrafilter/rss.xsl >test.rss
 
 aws s3 cp test.rss s3://www.neal.news/ultrafilter4.rss
 
